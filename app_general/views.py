c660ce1f08996f1c7135eebd411c8baaa3cd32c8
Hery <thitphavanh.psv@gmail.com>
2022-06-04 14:32:10 +0700

    update

--- app_general/views.py
+++ app_general/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('Phenomenal')
