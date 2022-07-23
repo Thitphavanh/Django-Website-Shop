b64a1e854c0cb34d3a5b4b331dbc677c923eb9f3
Hery <thitphavanh.psv@gmail.com>
2022-07-23 10:24:57 +0700

    update

--- app_general/views.py
+++ app_general/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>PHENOMENAL</h1>')
+    return render(request, 'app_general/home.html')
+
 
 def about(request):
     return HttpResponse('ABOUT PHENOMENAL')
