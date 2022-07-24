e8a2e47aad4c8c133f8bb4e1fc3d61dab9bcf523
Hery <thitphavanh.psv@gmail.com>
2022-07-24 16:26:52 +0700

    update

--- app_books/views.py
+++ app_books/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+
+all_books = [
+    {'id': 1, 'title': 'Steve Jobs by Walter Isaacson', 'price': 320000, 'is_hardcover': True},
+    {'id': 2, 'title': 'Elon Musk', 'price': 120000, 'is_hardcover': False},
+    {'id': 3, 'title': 'Leonardo da Vinci by Walter Isaacson', 'price': 320000, 'is_hardcover': True},
+    {'id': 4, 'title': 'Einstein, His Life and Universe', 'price': 320000, 'is_hardcover': False},
+
+]
+
+
 def books(request):
-    return render(request, 'app_books/books.html')
+    context = { 'books': all_books}
+    return render(request, 'app_books/books.html',context)
+
 
 def book(request, book_id):
-    return render(request, 'app_books/book.html', context={'book_id': book_id })
+    return render(request, 'app_books/book.html', context={'book_id': book_id})
--- app_general/views.py
+++ app_general/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+
 def home(request):
     return render(request, 'app_general/home.html')
 
